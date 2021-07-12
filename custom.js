69212b3a974609bdab869d40f5bf95529f0ce914
Md. Jawad Jamil <md.jawadjamil@gmail.com>
2021-07-12 13:57:57 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,258 @@
+
+function grading_bang(number) {
+    
+if (0>number || number>100) {
+    alert('Please Check Your Number')
+}
+
+
+else if(0<=number && number<33){
+    document.getElementById('res_heading_bang').innerHTML = "Bangla: You Are Fail";
+}
+
+
+
+else if (33<=number && number<40) {
+    document.getElementById('res_heading_bang').innerHTML = 'Bangla: D';
+}
+
+
+else if (40<=number && number<50) {
+    document.getElementById('res_heading_bang').innerHTML = "Bangla: C";
+}
+
+else if (50<=number && number<60) {
+    document.getElementById('res_heading_bang').innerHTML = "Bangla: B";
+}
+
+else if (60<=number && number<70) {
+    document.getElementById('res_heading_bang').innerHTML = "Bangla: A-";
+}
+
+else if (70<=number && number<80) {
+    document.getElementById('res_heading_bang').innerHTML = "Bangla: A";
+}
+
+
+else if (80<=number && number<=100) {
+    document.getElementById('res_heading_bang').innerHTML = "Bangla: A+";
+}
+
+}
+
+
+
+function grading_eng(number) {
+    
+if (0>number || number>100) {
+    alert('Please Check Your Number')
+}
+
+
+else if(0<=number && number<33){
+    document.getElementById('res_heading_eng').innerHTML = "English: You Are Fail";
+}
+
+
+
+else if (33<=number && number<40) {
+    document.getElementById('res_heading_eng').innerHTML = 'English: D';
+}
+
+
+else if (40<=number && number<50) {
+    document.getElementById('res_heading_eng').innerHTML = "English: C";
+}
+
+else if (50<=number && number<60) {
+    document.getElementById('res_heading_eng').innerHTML = "English: B";
+}
+
+else if (60<=number && number<70) {
+    document.getElementById('res_heading_eng').innerHTML = "English: A-";
+}
+
+else if (70<=number && number<80) {
+    document.getElementById('res_heading_eng').innerHTML = "English: A";
+}
+
+
+else if (80<=number && number<=100) {
+    document.getElementById('res_heading_eng').innerHTML = "English: A+";
+}
+
+
+}
+
+
+function grading_phy(number) {
+    
+if (0>number || number>100) {
+    alert('Please Check Your Number')
+}
+
+
+else if(0<=number && number<33){
+    document.getElementById('res_heading_phy').innerHTML = "Physics: You Are Fail";
+}
+
+
+
+else if (33<=number && number<40) {
+    document.getElementById('res_heading_phy').innerHTML = 'Physics: D';
+}
+
+
+else if (40<=number && number<50) {
+    document.getElementById('res_heading_phy').innerHTML = "Physics: C";
+}
+
+else if (50<=number && number<60) {
+    document.getElementById('res_heading_phy').innerHTML = "Physics: B";
+}
+
+else if (60<=number && number<70) {
+    document.getElementById('res_heading_phy').innerHTML = "Physics: A-";
+}
+
+else if (70<=number && number<80) {
+    document.getElementById('res_heading_phy').innerHTML = "Physics: A";
+}
+
+
+else if (80<=number && number<=100) {
+    document.getElementById('res_heading_phy').innerHTML = "Physics: A+";
+}
+
+
+}
+
+
+function grading_bio(number) {
+    
+if (0>number || number>100) {
+    alert('Please Check Your Number')
+}
+
+
+else if(0<=number && number<33){
+    document.getElementById('res_heading_bio').innerHTML = "Biology: You Are Fail";
+}
+
+
+
+else if (33<=number && number<40) {
+    document.getElementById('res_heading_bio').innerHTML = 'Biology: D';
+}
+
+
+else if (40<=number && number<50) {
+    document.getElementById('res_heading_bio').innerHTML = "Biology: C";
+}
+
+else if (50<=number && number<60) {
+    document.getElementById('res_heading_bio').innerHTML = "Biology: B";
+}
+
+else if (60<=number && number<70) {
+    document.getElementById('res_heading_bio').innerHTML = "Biology: A-";
+}
+
+else if (70<=number && number<80) {
+    document.getElementById('res_heading_bio').innerHTML = "Biology: A";
+}
+
+
+else if (80<=number && number<=100) {
+    document.getElementById('res_heading_bio').innerHTML = "Biology: A+";
+}
+
+
+}
+
+
+function grading_che(number) {
+    
+if (0>number || number>100) {
+    alert('Please Check Your Number')
+}
+
+
+else if(0<=number && number<33){
+    document.getElementById('res_heading_che').innerHTML = "Chemistry: You Are Fail";
+}
+
+
+
+else if (33<=number && number<40) {
+    document.getElementById('res_heading_che').innerHTML = 'Chemistry: D';
+}
+
+
+else if (40<=number && number<50) {
+    document.getElementById('res_heading_che').innerHTML = "Chemistry: C";
+}
+
+else if (50<=number && number<60) {
+    document.getElementById('res_heading_che').innerHTML = "Chemistry: B";
+}
+
+else if (60<=number && number<70) {
+    document.getElementById('res_heading_che').innerHTML = "Chemistry: A-";
+}
+
+else if (70<=number && number<80) {
+    document.getElementById('res_heading_che').innerHTML = "Chemistry: A";
+}
+
+
+else if (80<=number && number<=100) {
+    document.getElementById('res_heading_che').innerHTML = "Chemistry: A+";
+}
+
+
+}
+
+
+
+function grading_reli(number) {
+    
+if (0>number || number>100) {
+    alert('Please Check Your Number')
+}
+
+
+else if(0<=number && number<33){
+    document.getElementById('res_heading_reli').innerHTML = "Religion: You Are Fail";
+}
+
+
+
+else if (33<=number && number<40) {
+    document.getElementById('res_heading_reli').innerHTML = 'Religion: D';
+}
+
+
+else if (40<=number && number<50) {
+    document.getElementById('res_heading_reli').innerHTML = "Religion: C";
+}
+
+else if (50<=number && number<60) {
+    document.getElementById('res_heading_reli').innerHTML = "Religion: B";
+}
+
+else if (60<=number && number<70) {
+    document.getElementById('res_heading_reli').innerHTML = "Religion: A-";
+}
+
+else if (70<=number && number<80) {
+    document.getElementById('res_heading_reli').innerHTML = "Religion: A";
+}
+
+
+else if (80<=number && number<=100) {
+    document.getElementById('res_heading_reli').innerHTML = "Religion: A+";
+}
+
+
+}
